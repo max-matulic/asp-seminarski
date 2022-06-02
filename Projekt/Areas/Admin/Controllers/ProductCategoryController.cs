5039ac24196f0603ba4c938dbddd816e64323d0f
Max Matulic <maxmatuli@gmail.com>
2022-06-02 18:54:29 +0200

    extended ProductCategoryController

--- Projekt/Areas/Admin/Controllers/ProductCategoryController.cs
+++ Projekt/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Projekt.Data;
 using Projekt.Models;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Projekt.Areas.Admin.Controllers
@@ -32,6 +34,87 @@ namespace Projekt.Areas.Admin.Controllers
             ViewBag.ProductId = productId;
 
             return View(productCategories);
+        }
+
+        public IActionResult Create(int productId)
+        {
+            ViewBag.ProductId = productId;
+            ViewBag.Categories = GetCategories();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductCategory productCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.ProductCategory.Add(productCategory);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction(nameof(Index), new { productId = productCategory.ProductId });
+            }
+
+            return View(productCategory);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var productCategory = _dbContext.ProductCategory.FirstOrDefault(p => p.Id == id);
+            ViewBag.Categories = GetCategories();
+
+            return View(productCategory);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ProductCategory productCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.ProductCategory.Update(productCategory);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction(nameof(Index), new { productId = productCategory.ProductId });
+            }
+
+            return View(productCategory);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var productCategories = (from prodCat in _dbContext.ProductCategory
+                                     where prodCat.Id == id
+                                     select new ProductCategory
+                                     {
+                                         Id = prodCat.Id,
+                                         ProductId = prodCat.ProductId,
+                                         ProductTitle = (from p in _dbContext.Product where p.Id == prodCat.ProductId select p.Title).FirstOrDefault(),
+                                         CategoryId = prodCat.CategoryId,
+                                         CategoryTitle = (from c in _dbContext.Category where c.Id == prodCat.CategoryId select c.Title).FirstOrDefault()
+                                     }).SingleOrDefault();
+
+            return View(productCategories);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteProductCategory(int id)
+        {
+            var prodCat = _dbContext.ProductCategory.Find(id);
+            _dbContext.ProductCategory.Remove(prodCat);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index), new { productId = prodCat.ProductId });
+        }
+
+
+
+        private List<SelectListItem> GetCategories()
+        {
+            return _dbContext.Category.Select(c => new SelectListItem()
+            {
+                Value = c.Id.ToString(),
+                Text = c.Title
+            }).ToList();
         }
     }
 }
